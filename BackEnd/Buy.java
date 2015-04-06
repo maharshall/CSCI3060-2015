5dee5428c88e21ef8c7c4821d38c5278caeb2a4b
mysteryhbo <thepeterlittle@gmail.com>
2015-04-06 16:54:58 -0400

    removed price in Buy.apply()

--- BackEnd/Buy.java
+++ BackEnd/Buy.java
@@ -1,14 +1,9 @@
 //For buying tickets
 
-import java.io.*;
-import java.util.*;
-
 public class Buy {
     public Buy(){}
 
     public void apply(User seller, Ticket event, int tix){
-    	int price = event.getPrice();
-    	
     	event.updateTick(-tix);
     }
 }
